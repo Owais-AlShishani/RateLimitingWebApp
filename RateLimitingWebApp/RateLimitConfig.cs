131b17388fee505221ecea16691197e1e32b6092
Owais-Alshishani <Awais.alshishani@gmail.com>
2024-07-14 13:33:15 +0300

    Add project files.

---
+++ RateLimitingWebApp/RateLimitConfig.cs
@@ -0,0 +1,15 @@
+﻿using AspNetCoreRateLimit;
+namespace RateLimitingWebApp
+{
+    public static class RateLimitConfig
+    {
+        public static void AddRateLimitServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<IpRateLimitOptions>(configuration.GetSection("IpRateLimiting"));
+
+            services.AddInMemoryRateLimiting();
+
+            services.AddSingleton<IRateLimitConfiguration, RateLimitConfiguration>();
+        }
+    }
+}
